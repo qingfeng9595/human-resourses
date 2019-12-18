49d776d4c986284e27efca5449348a314a5f7f72
qingfeng9595 <qingfeng9595@outlook.com>
2019-12-18 23:49:06 +0800

    考勤汇总中英文

--- src/locales/en-US/menu.js
+++ src/locales/en-US/menu.js
@@ -22,6 +22,7 @@ export default {
   'menu.form.advanced-form': 'Advanced Form',
   'menu.list': 'List',
   'menu.list.table-list': 'Search Table',
+  'menu.record-list': 'Record Table',
   'menu.list.basic-list': 'Basic List',
   'menu.list.card-list': 'Card List',
   'menu.list.search-list': 'Search List',
